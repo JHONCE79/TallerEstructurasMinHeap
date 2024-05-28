01dbae9445c57ee94e96d723ae230feb0e6f8315
Steven <jhonsteven.163@gmail.com>
2024-05-28 06:43:22 -0500

    final_3

--- Arboles_MinHeap.py
+++ Arboles_MinHeap.py
@@ -1,6 +1,3 @@
-import tkinter as tk
-from tkinter import messagebox
-
 class Paciente:
     def __init__(self, numero_paciente, genero, nombre, edad, triaje):
         self.numero_paciente = numero_paciente
@@ -9,172 +6,248 @@ class Paciente:
         self.edad = edad
         self.triaje = triaje
 
+
+class Nodo:
+    def __init__(self, paciente):
+        self.paciente = paciente
+        self.izquierda = None
+        self.derecha = None
+        self.padre = None
+
+
 class HeapMin:
     def __init__(self):
-        self.heap = []
+        self.raiz = None
+        self.size = 0
 
     def insertar(self, paciente):
-        self.heap.append(paciente)
-        self._heapify_up(len(self.heap) - 1)
+        nuevo_nodo = Nodo(paciente)
+        self.size += 1
+        if self.raiz is None:
+            self.raiz = nuevo_nodo
+        else:
+            self._insertar_nodo(self.raiz, nuevo_nodo)
+            self._heapify_up(nuevo_nodo)
 
-    def _heapify_up(self, index):
-        parent_index = (index - 1) // 2
-        if parent_index >= 0 and self.heap[index].triaje < self.heap[parent_index].triaje:
-            self.heap[index], self.heap[parent_index] = self.heap[parent_index], self.heap[index]
-            self._heapify_up(parent_index)
+    def _insertar_nodo(self, nodo, nuevo_nodo):
+        queue = [nodo]
+        while queue:
+            actual = queue.pop(0)
+            if actual.izquierda is None:
+                actual.izquierda = nuevo_nodo
+                nuevo_nodo.padre = actual
+                return
+            elif actual.derecha is None:
+                actual.derecha = nuevo_nodo
+                nuevo_nodo.padre = actual
+                return
+            else:
+                queue.append(actual.izquierda)
+                queue.append(actual.derecha)
+
+    def _heapify_up(self, nodo):
+        while nodo.padre and nodo.paciente.triaje < nodo.padre.paciente.triaje:
+            nodo.paciente, nodo.padre.paciente = nodo.padre.paciente, nodo.paciente
+            nodo = nodo.padre
 
     def consultar_proximo(self):
-        if self.heap:
-            return self.heap[0]
-        else:
-            return None
+        return self.raiz.paciente if self.raiz else None
 
     def atender_siguiente(self):
-        if not self.heap:
+        if not self.raiz:
             return None
-        if len(self.heap) == 1:
-            return self.heap.pop()
-        root = self.heap[0]
-        self.heap[0] = self.heap.pop()
-        self._heapify_down(0)
-        return root
+        if self.size == 1:
+            paciente = self.raiz.paciente
+            self.raiz = None
+            self.size -= 1
+            return paciente
 
-    def _heapify_down(self, index):
-        smallest = index
-        left_child_index = 2 * index + 1
-        right_child_index = 2 * index + 2
+        root_paciente = self.raiz.paciente
+        last_node = self._get_last_node()
+        self.raiz.paciente = last_node.paciente
+        if last_node.padre:
+            if last_node.padre.derecha == last_node:
+                last_node.padre.derecha = None
+            else:
+                last_node.padre.izquierda = None
+        self._heapify_down(self.raiz)
+        self.size -= 1
+        return root_paciente
 
-        if left_child_index < len(self.heap) and self.heap[left_child_index].triaje < self.heap[smallest].triaje:
-            smallest = left_child_index
+    def _heapify_down(self, nodo):
+        while nodo:
+            smallest = nodo
+            if nodo.izquierda and nodo.izquierda.paciente.triaje < smallest.paciente.triaje:
+                smallest = nodo.izquierda
+            if nodo.derecha and nodo.derecha.paciente.triaje < smallest.paciente.triaje:
+                smallest = nodo.derecha
+            if smallest == nodo:
+                break
+            nodo.paciente, smallest.paciente = smallest.paciente, nodo.paciente
+            nodo = smallest
 
-        if right_child_index < len(self.heap) and self.heap[right_child_index].triaje < self.heap[smallest].triaje:
-            smallest = right_child_index
-
-        if smallest != index:
-            self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
-            self._heapify_down(smallest)
+    def _get_last_node(self):
+        queue = [self.raiz]
+        last_node = None
+        while queue:
+            last_node = queue.pop(0)
+            if last_node.izquierda:
+                queue.append(last_node.izquierda)
+            if last_node.derecha:
+                queue.append(last_node.derecha)
+        return last_node
 
     def consultar_pacientes_espera(self):
-        return "\n".join([f"Número: {p.numero_paciente} | Nombre: {p.nombre} | Triaje: {p.triaje}" for p in self.heap])
+        pacientes_info = self._recorrer_arbol(self.raiz)
+        return pacientes_info
+
+    def _recorrer_arbol(self, nodo):
+        if not nodo:
+            return ""
+        pacientes_info = ""
+        queue = [nodo]
+        while queue:
+            actual = queue.pop(0)
+            paciente = actual.paciente
+            pacientes_info += f"Número: {paciente.numero_paciente} | Nombre: {paciente.nombre} | Triaje: {paciente.triaje}\n"
+            if actual.izquierda:
+                queue.append(actual.izquierda)
+            if actual.derecha:
+                queue.append(actual.derecha)
+        return pacientes_info
 
     def consultar_pacientes_por_triaje(self, triaje):
-        return "\n".join([f"Nombre: {p.nombre} | Triaje: {p.triaje}" for p in self.heap if p.triaje == triaje])
+        pacientes_info = self._buscar_y_mostrar_por_triaje(self.raiz, triaje)
+        return pacientes_info
+
+    def _buscar_y_mostrar_por_triaje(self, nodo, triaje):
+        if not nodo:
+            return ""
+        pacientes_info = ""
+        queue = [nodo]
+        while queue:
+            actual = queue.pop(0)
+            if actual.paciente.triaje == triaje:
+                pacientes_info += f"Nombre: {actual.paciente.nombre} | Triaje: {actual.paciente.triaje}\n"
+            if actual.izquierda:
+                queue.append(actual.izquierda)
+            if actual.derecha:
+                queue.append(actual.derecha)
+        return pacientes_info
 
     def eliminar_paciente_cola(self, nombre):
-        for i, paciente in enumerate(self.heap):
-            if paciente.nombre == nombre:
-                last_paciente = self.heap.pop()
-                if i < len(self.heap):
-                    self.heap[i] = last_paciente
-                    self._heapify_down(i)
-                    self._heapify_up(i)
-                break
+        self.raiz = self._eliminar_nodo_por_nombre(self.raiz, nombre)
 
-class ColaPacientesApp:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Cola de Pacientes")
+    def _eliminar_nodo_por_nombre(self, nodo, nombre):
+        if not nodo:
+            return None
+        if nodo.paciente.nombre == nombre:
+            self.size -= 1
+            last_node = self._get_last_node()
+            if nodo == last_node:
+                return None
+            nodo.paciente = last_node.paciente
+            if last_node.padre:
+                if last_node.padre.derecha == last_node:
+                    last_node.padre.derecha = None
+                else:
+                    last_node.padre.izquierda = None
+            self._heapify_down(nodo)
+            return nodo
+        nodo.izquierda = self._eliminar_nodo_por_nombre(nodo.izquierda, nombre)
+        nodo.derecha = self._eliminar_nodo_por_nombre(nodo.derecha, nombre)
+        return nodo
 
+
+class ColaPacientesConsola:
+    def __init__(self):
         self.cola_pacientes = HeapMin()
+        self.menu()
 
-        self.create_widgets()
+    def menu(self):
+        while True:
+            print("\nCola de Pacientes")
+            print("1. Registrar Paciente")
+            print("2. Consultar Próximo Paciente")
+            print("3. Atender Siguiente Paciente")
+            print("4. Consultar Pacientes en Espera")
+            print("5. Consultar Pacientes por Triaje")
+            print("6. Eliminar Paciente")
+            print("7. Salir")
+            opcion = input("Seleccione una opción: ")
 
-    def create_widgets(self):
-        tk.Label(self.root, text="Datos del Paciente").grid(row=0, column=0, columnspan=2)
-        tk.Label(self.root, text="Número de Paciente:").grid(row=1, column=0)
-        self.num_paciente_entry = tk.Entry(self.root)
-        self.num_paciente_entry.grid(row=1, column=1)
-        tk.Label(self.root, text="Género:").grid(row=2, column=0)
-        self.genero_entry = tk.Entry(self.root)
-        self.genero_entry.grid(row=2, column=1)
-        tk.Label(self.root, text="Nombre:").grid(row=3, column=0)
-        self.nombre_entry = tk.Entry(self.root)
-        self.nombre_entry.grid(row=3, column=1)
-        tk.Label(self.root, text="Edad:").grid(row=4, column=0)
-        self.edad_entry = tk.Entry(self.root)
-        self.edad_entry.grid(row=4, column=1)
-        tk.Label(self.root, text="Triaje:").grid(row=5, column=0)
-        self.triaje_entry = tk.Entry(self.root)
-        self.triaje_entry.grid(row=5, column=1)
-        tk.Label(self.root, text="Eliminar Paciente").grid(row=12, column=0, columnspan=2)
-        tk.Label(self.root, text="Nombre del Paciente:").grid(row=13, column=0)
-        self.eliminar_nombre_entry = tk.Entry(self.root)
-        self.eliminar_nombre_entry.grid(row=13, column=1)
-
-        tk.Button(self.root, text="Registrar Paciente", command=self.registrar_paciente).grid(row=6, column=0,
-                                                                                              columnspan=2)
-
-        tk.Button(self.root, text="Consultar Próximo Paciente", command=self.consultar_proximo_paciente).grid(row=7,
-                                                                                                              column=0,
-                                                                                                              columnspan=2)
-
-        tk.Button(self.root, text="Atender Siguiente Paciente", command=self.atender_siguiente_paciente).grid(row=8,
-                                                                                                              column=0,
-                                                                                                              columnspan=2)
-
-        tk.Button(self.root, text="Consultar Pacientes en Espera", command=self.consultar_pacientes_espera).grid(row=9,
-                                                                                                                 column=0,
-                                                                                                                 columnspan=2)
-
-        tk.Button(self.root, text="Consultar Pacientes por Triaje", command=self.consultar_pacientes_por_triaje).grid(
-            row=10, column=0, columnspan=2)
-
-        tk.Button(self.root, text="Eliminar Paciente", command=self.eliminar_paciente).grid(row=14, column=0,
-                                                                                            columnspan=2)
+            if opcion == '1':
+                self.registrar_paciente()
+            elif opcion == '2':
+                self.consultar_proximo_paciente()
+            elif opcion == '3':
+                self.atender_siguiente_paciente()
+            elif opcion == '4':
+                self.consultar_pacientes_espera()
+            elif opcion == '5':
+                self.consultar_pacientes_por_triaje()
+            elif opcion == '6':
+                self.eliminar_paciente()
+            elif opcion == '7':
+                break
+            else:
+                print("Opción no válida. Por favor, intente de nuevo.")
 
     def registrar_paciente(self):
         try:
-            numero_paciente = int(self.num_paciente_entry.get())
-            genero = self.genero_entry.get()
-            nombre = self.nombre_entry.get()
-            edad = int(self.edad_entry.get())
-            triaje = int(self.triaje_entry.get())
+            numero_paciente = int(input("Número de Paciente: "))
+            genero = input("Género: ")
+            nombre = input("Nombre: ")
+            edad = int(input("Edad: "))
+            triaje = int(input("Triaje: "))
             paciente = Paciente(numero_paciente, genero, nombre, edad, triaje)
             self.cola_pacientes.insertar(paciente)
-            messagebox.showinfo("Éxito", "Paciente registrado exitosamente.")
+            print("Paciente registrado exitosamente.")
         except ValueError:
-            messagebox.showerror("Error", "Por favor, ingrese datos válidos.")
+            print("Error: Por favor, ingrese datos válidos.")
 
     def consultar_proximo_paciente(self):
         proximo_paciente = self.cola_pacientes.consultar_proximo()
         if proximo_paciente:
-            messagebox.showinfo("Próximo Paciente", f"Próximo paciente a ser atendido: {proximo_paciente.nombre}")
+            print(f"Próximo paciente a ser atendido: {proximo_paciente.nombre}")
         else:
-            messagebox.showinfo("Próximo Paciente", "No hay pacientes en espera.")
+            print("No hay pacientes en espera.")
 
     def atender_siguiente_paciente(self):
         paciente_atendido = self.cola_pacientes.atender_siguiente()
         if paciente_atendido:
-            messagebox.showinfo("Paciente Atendido", f"Paciente atendido: {paciente_atendido.nombre}")
+            print(f"Paciente atendido: {paciente_atendido.nombre}")
         else:
-            messagebox.showinfo("Paciente Atendido", "No hay pacientes en espera.")
+            print("No hay pacientes en espera.")
 
     def consultar_pacientes_espera(self):
         pacientes_espera_info = self.cola_pacientes.consultar_pacientes_espera()
         if pacientes_espera_info:
-            messagebox.showinfo("Pacientes en Espera", pacientes_espera_info)
+            print("Pacientes en espera:")
+            print(pacientes_espera_info)
         else:
-            messagebox.showinfo("Pacientes en Espera", "No hay pacientes en espera.")
+            print("No hay pacientes en espera.")
 
     def consultar_pacientes_por_triaje(self):
         try:
-            triaje = int(self.triaje_entry.get())
+            triaje = int(input("Triaje: "))
             pacientes_info = self.cola_pacientes.consultar_pacientes_por_triaje(triaje)
             if pacientes_info:
-                messagebox.showinfo("Pacientes por Triaje", pacientes_info)
+                print("Pacientes con triaje", triaje, ":")
+                print(pacientes_info)
             else:
-                messagebox.showinfo("Pacientes por Triaje", f"No hay pacientes en espera con triaje {triaje}.")
+                print(f"No hay pacientes en espera con triaje {triaje}.")
         except ValueError:
-            messagebox.showerror("Error", "Por favor, ingrese un triaje válido.")
+            print("Error: Por favor, ingrese un triaje válido.")
 
     def eliminar_paciente(self):
-        nombre = self.eliminar_nombre_entry.get()
+        nombre = input("Nombre del Paciente a eliminar: ")
         if nombre:
             self.cola_pacientes.eliminar_paciente_cola(nombre)
-            messagebox.showinfo("Paciente Eliminado", f"Paciente {nombre} eliminado exitosamente.")
+            print(f"Paciente {nombre} eliminado exitosamente.")
         else:
-            messagebox.showerror("Error", "Por favor, ingrese el nombre del paciente a eliminar.")
+            print("Error: Por favor, ingrese el nombre del paciente a eliminar.")
 
-root = tk.Tk()
-app = ColaPacientesApp(root)
-root.mainloop()
+
+if __name__ == "__main__":
+    ColaPacientesConsola()
